af7a6da6cca8151757575e11228cb2931f55ab46
fagelogo <fagelogo@gmail.com>
2021-10-14 08:03:04 +0000

    add basic java script

---
+++ music app.js
@@ -0,0 +1,98 @@
+window.onload = () => {
+    const albumart = document.querySelector('#albumart');
+    const song_title = document.querySelector('#title');
+    const song_artist = document.querySelector('#artist');
+
+    const play_btn = document.querySelector('#list');
+    // const play_btn_icon = document.querySelector('#play-btn .play-icon');
+    const prev_btn = document.querySelector('#previous');
+    const next_btn = document.querySelector('#next');
+    // const duration = document.querySelector('#seek');
+
+    const audio_player = document.querySelector('#player');
+
+    
+
+    let current_song_index;
+    let next_song_index;
+
+    const songs = [
+    {          
+            title: 'Courage',
+                    artist: "Celine Dion",
+                    song_path: 'music/celine_dion_courage.mp3',
+                    img_path: 'images/celine.png'
+                },
+                {
+                    title: 'Josh Turner',
+                    artist: "Your Man",
+                    song_path: 'music/your_man_by_josh_turner.mp3',
+                    img_path: 'images/josh.jpg'
+                },
+            ];
+
+            function TogglePlaySong () {
+                if (audio_player.paused) {
+                    audio_player.play();
+                    // play_btn_icon.classList.remove('fa-play')
+                    // play_btn_icon.classList.add('fa-pause');
+                } else {
+                    audio_player.pause();
+                    // play_btn_icon.classList.add('fa-play')
+                    // play_btn_icon.classList.remove('fa-pause');
+                }
+            }
+
+            
+            play_btn.addEventListener('click', TogglePlaySong());
+    next_btn.addEventListener('click', () => ChangeSong());
+    prev_btn.addEventListener('click', () => ChangeSong(false));
+    
+
+    
+
+        function InitPlayer () {
+            current_song_index = 0;
+            next_song_index = current_song_index + 1;
+            UpdatePlayer();
+        }
+        InitPlayer();
+
+        
+        function UpdatePlayer() {
+            let song = songs[current_song_index];
+    
+            albumart.style = "background-image: url('" + song.img_path + "');";
+            song_title.innerText = song.title;
+            song_artist.innerText = song.artist;
+            audio_player.src = song.song_path;
+        }
+        function ChangeSong (next = true) {
+            if (next) {
+                current_song_index++;
+                next_song_index = current_song_index + 1;
+    
+                if (current_song_index > songs.length - 1) {
+                    current_song_index = 0;
+                    next_song_index = current_song_index+1;
+                }
+    
+                if (next_song_index > songs.length - 1) {
+                    next_song_index = 0;
+                }
+            } else {
+                current_song_index--;
+                next_song_index = current_song_index + 1;
+    
+                if (current_song_index < 0) {
+                    current_song_index = songs.length - 1;
+                    next_song_index = 0;
+                }
+            }
+    
+            UpdatePlayer();
+            TogglePlaySong();
+        }
+    
+        
+    }
